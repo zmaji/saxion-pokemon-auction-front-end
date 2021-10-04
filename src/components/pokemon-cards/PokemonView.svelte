4c781d90abc3be74394ee0deda87d6606cfc3eed
zmaji <502241@student.nl>
2021-10-04 11:54:41 +0200

    Added bids and minor adjustments to PokemonView modal

--- src/components/pokemon-cards/PokemonView.svelte
+++ src/components/pokemon-cards/PokemonView.svelte
@@ -17,7 +17,7 @@
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Display the result in the element with id="demo"
+        // Display the result in the element with id="CountdownTimer"
         document.getElementById("CountdownTimer").innerHTML = days + "d " + hours + "h "
             + minutes + "m " + seconds + "s ";
 
@@ -27,6 +27,20 @@
             document.getElementById("CountdownTimer").innerHTML = "EXPIRED";
         }
     }, 1000);
+
+    export let bid = {};
+
+    const fetchBidOwner = (async () => {
+        console.log('Bearer ' + localStorage.getItem('token'))
+        const response = await fetch(`http://localhost:3000/users/${bid.userID}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        });
+        return await response.json();
+    })();
 </script>
 
 <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="viewModal" aria-hidden="true">
@@ -34,6 +48,7 @@
         <div class="modal-content rounded">
             <div class="modal-header text-center">
                 <h3 class="w-100">Auction Title</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body py-4">
                 <div class="row">
@@ -59,6 +74,27 @@
                         <h6>Available until: Date</h6>
                         <i class="fas fa-hourglass-half pb-2"></i>
                         <p id="CountdownTimer"></p>
+                    </div>
+                    <h3 class="pt-2 ps-3">Bids:</h3>
+                    <div class="col-12 px-3 py-2">
+                        <div class="{bid.hasWon ? 'alert alert-primary border-2 border-primary' : 'alert alert-secondary'} rounded shadow-sm p-3 mb-3">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h4 class="mb-0">€ test</h4>
+                                <small class="">Placed by: test</small>
+                            </div>
+                        </div>
+                        <div class="{bid.hasWon ? 'alert alert-primary border-2 border-primary' : 'alert alert-secondary'} rounded shadow-sm p-3 mb-3">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h4 class="mb-0">€ test</h4>
+                                <small class="">Placed by: test</small>
+                            </div>
+                        </div>
+                        <div class="{bid.hasWon ? 'alert alert-primary border-2 border-primary' : 'alert alert-secondary'} rounded shadow-sm p-3">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h4 class="mb-0">€ test</h4>
+                                <small class="">Placed by: test</small>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
