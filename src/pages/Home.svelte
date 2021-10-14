635ab140833023c3084dd8c2fb8ab0f574446efc
NKimenai <nilskimenai@hotmail.nl>
2021-10-14 20:58:22 +0200

    start on homepage with base setup for walkthrough of app

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,35 @@
 <h1>The pokemon card auction homepage</h1>
+<hr>
+
+<h3>How it works</h3>
+<p>Explanation</p>
+
+<h3>Available functions</h3>
+<p>Global Explanation</p>
+
+<h4>Pokemon cards</h4>
+<p>Explanation</p>
+
+<h4>Bids</h4>
+<p>Explanation</p>
+
+<h4>Register</h4>
+<p>Explanation</p>
+
+<h4>Login</h4>
+<p>Explanation</p>
+<ul>
+    <li class="mb-2"><span class="font-monospace bg-light px-2 py-1 fw-bold">User password</span></li>
+    <li class="mb-2"><span class="font-monospace bg-light px-2 py-1 fw-bold">User email</span></li>
+    <li class="mb-2"><span class="font-monospace bg-light px-2 py-1 fw-bold">User token</span></li>
+</ul>
+
+<h3>Roles</h3>
+<p>The currently implemented roles:</p>
+
+<ul>
+    <li class="mb-2"><span class="font-monospace bg-light px-2 py-1 fw-bold">Admin</span></li>
+    <li class="mb-2"><span class="font-monospace bg-light px-2 py-1 fw-bold">User</span></li>
+</ul>
+
+<p>Explanation</p>
