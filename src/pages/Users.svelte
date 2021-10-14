706de4839e1df4c5d0fd51427162d540593d4983
zmaji <502241@student.saxion.nl>
2021-10-14 12:00:59 +0200

    Usercard changes

--- src/pages/Users.svelte
+++ src/pages/Users.svelte
@@ -1,5 +1,33 @@
 <script>
+    import { fade } from 'svelte/transition';
     import UserCard from "../components/users/UserCard.svelte";
+
+    const fetchUsers = (async () => {
+        console.log('Bearer ' + localStorage.getItem('token'))
+        const response = await fetch(`http://localhost:3000/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        });
+        return await response.json();
+    })();
 </script>
 
-<UserCard></UserCard>
+{#await fetchUsers}
+    <div class="alert alert-primary" role="alert">
+        Fetching Users...
+    </div>
+{:then users}
+    <div class="row" transition:fade>
+        {#each users as user}
+            <UserCard {user}/>
+        {/each}
+    </div>
+{:catch error}
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        Failed to fetch Users...
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+{/await}
